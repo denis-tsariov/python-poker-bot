bc141a2d5018b683e745ed5fd31f377618d873a2
erichugy <eric.huang3143@gmail.com>
2025-02-08 18:38:10 -0500

    Created naive percentile range

---
+++ tg/max_eric_bot.py
@@ -0,0 +1,130 @@
+import asyncio
+from typing import Tuple
+import argparse
+import treys
+import time
+from collections import defaultdict
+
+from tg.bot import Bot
+import tg.types as pokerTypes
+
+parser = argparse.ArgumentParser(
+    prog='Template bot',
+    description='A Turing Games poker bot that always checks or calls, no matter what the target bet is (it never folds and it never raises)')
+
+parser.add_argument('--port', type=int,
+                    help='The port to connect to the server on')
+parser.add_argument('--host', type=str, default='localhost',
+                    help='The host to connect to the server on')
+parser.add_argument('--room', type=str, default='my-new-room',
+                    help='The room to connect to')
+parser.add_argument('--party', type=str, default='poker',
+                    help='The room to connect to')
+parser.add_argument('--key', type=str, default='',
+                    help='The key for authentication')
+parser.add_argument('--simulations', type=int, default=900)
+
+args = parser.parse_args()
+
+def card_name(card: pokerTypes.Card):
+    val = str(card.rank)
+    if card.rank == 1:
+        val = 'A'
+    if card.rank == 10:
+        val = 'T'
+    elif card.rank == 11:
+        val = 'J'
+    elif card.rank == 12:
+        val = 'Q'
+    elif card.rank == 13:
+        val = 'K'
+    return f"{val}{card.suit[0]}"
+
+
+poker_hand_percentiles = {
+    "AA": 1, "AKs": 2, "AQs": 2, "AJs": 3, "ATs": 5, "A9s": 8, "A8s": 10, "A7s": 13, "A6s": 14, "A5s": 12, "A4s": 14, "A3s": 14, "A2s": 17,
+    "AKo": 1, "KK": 1, "KQs": 3, "KJs": 3, "KTs": 6, "K9s": 10, "K8s": 16, "K7s": 19, "K6s": 24, "K5s": 25, "K4s": 25, "K3s": 26, "K2s": 26,
+    "AQo": 8, "KQo": 9, "QQ": 1, "QJs": 5, "QTs": 6, "Q9s": 10, "Q8s": 19, "Q7s": 26, "Q6s": 28, "Q5s": 29, "Q4s": 29, "Q3s": 30, "Q2s": 31,
+    "AJo": 12, "KJo": 14, "QJo": 15, "JJ": 2, "JTs": 6, "J9s": 11, "J8s": 17, "J7s": 27, "J6s": 33, "J5s": 35, "J4s": 37, "J3s": 37, "J2s": 38,
+    "ATo": 18, "KTo": 20, "QTo": 22, "JTo": 21, "TT": 4, "T9s": 10, "T8s": 16, "T7s": 25, "T6s": 31, "T5s": 40, "T4s": 40, "T3s": 41, "T2s": 41,
+    "A9o": 32, "K9o": 35, "Q9o": 36, "J9o": 34, "T9o": 31, "99": 7, "98s": 17, "97s": 24, "96s": 29, "95s": 38, "94s": 47, "93s": 47, "92s": 49,
+    "A8o": 39, "K8o": 50, "Q8o": 53, "J8o": 48, "T8o": 43, "98o": 42, "88": 9, "87s": 21, "86s": 27, "85s": 33, "84s": 40, "83s": 53, "82s": 54,
+    "A7o": 45, "K7o": 57, "Q7o": 66, "J7o": 64, "T7o": 59, "97o": 55, "87o": 52, "77": 12, "76s": 25, "75s": 28, "74s": 37, "73s": 45, "72s": 56,
+    "A6o": 51, "K6o": 60, "Q6o": 71, "J6o": 80, "T6o": 74, "96o": 68, "86o": 61, "76o": 57, "66": 16, "65s": 27, "64s": 29, "63s": 38, "62s": 49,
+    "A5o": 44, "K5o": 63, "Q5o": 75, "J5o": 82, "T5o": 89, "95o": 83, "85o": 73, "75o": 65, "65o": 58, "55": 20, "54s": 28, "53s": 32, "52s": 39,
+    "A4o": 46, "K4o": 67, "Q4o": 76, "J4o": 85, "T4o": 90, "94o": 95, "84o": 88, "74o": 78, "64o": 70, "54o": 62, "44": 23, "43s": 36, "42s": 41,
+    "A3o": 49, "K3o": 67, "Q3o": 77, "J3o": 86, "T3o": 92, "93o": 96, "83o": 98, "73o": 93, "63o": 81, "53o": 72, "43o": 76, "33": 23, "32s": 46,
+    "A2o": 54, "K2o": 69, "Q2o": 79, "J2o": 87, "T2o": 94, "92o": 97, "82o": 99, "72o": 100, "62o": 95, "52o": 84, "42o": 86, "32o": 91, "22": 24,
+}
+percentile_to_hands = defaultdict(list)
+for hand, percentile in poker_hand_percentiles.items():
+    percentile_to_hands[percentile].append(hand)
+sorted_percentiles = sorted(percentile_to_hands.keys())
+    
+
+
+class max_eric_bot(Bot):
+    fold_ = defaultdict(int)
+    call_ = defaultdict(int)
+    raise_ = defaultdict(int)
+    check_ = defaultdict(int)
+    total_ = defaultdict(int)
+    round_count = 0
+  
+    def act(self, state, hand):
+        # print('asked to act')
+        # print('acting', state, hand, self.my_id)
+        p = self.win_prob(state, hand)
+        EV_Call = p*(state.pot+state.target_bet)-(state.target_bet)*(1-p)
+        best_val = min(0, EV_Call)
+        action = 'fold' if best_val == 0 else 'call'
+        return {'type': action}
+
+    def opponent_action(self, action, player):
+        print(f'action: {action.type}')
+        print(f'folds count: {self.fold_}, raise count: {self.raise_}, call count: {self.call_}')
+        getattr(self, f'{action.type}_')[player.username] += 1
+        print('opponent action?', action, player)
+
+    def game_over(self, payouts):
+        print('game over', payouts)
+
+    def start_game(self, my_id):
+        self.my_id = my_id
+        print('start game', my_id)
+        self.round_count += 1
+        
+        
+    def win_prob(self, state: pokerTypes.PokerSharedState, hand: Tuple[pokerTypes.Card, pokerTypes.Card]):
+        out = 0
+        hand = [
+            treys.Card.new(card_name(hand[0])),
+            treys.Card.new(card_name(hand[1]))]
+        board = [treys.Card.new(card_name(card)) for card in state.cards]
+        evaluator = treys.Evaluator()
+        for i in range(args.simulations):
+            deck = treys.Deck()
+            deck.shuffle()
+            for card in hand+board:
+                deck.cards.remove(card)
+            pred = board + deck.draw(5-len(board))
+            score = evaluator.evaluate(hand, pred)
+            other = 10**9
+
+            for player in state.players:
+                if player.id != self.my_id:
+                    other = min(other, evaluator.evaluate(deck.draw(2), pred))
+            if score < other:
+                out += 1
+        return out/args.simulations
+    
+    def get_hands_in_percentile_range(self, min_percentile:int, max_percentile:int, min_threshold = 0, max_threshold = 60) -> list[str]:
+        hands = []
+        max_percentile = min(max_threshold, max_percentile)
+        for percentile in sorted_percentiles:
+            if min_percentile <= percentile <= max_percentile:
+                hands.extend(percentile_to_hands[percentile])
+        return hands
+
+    def get_player_max_percentile(self, player_id: str) -> int:
+        return self.fold_.get(player_id, 15) /self.round_count
