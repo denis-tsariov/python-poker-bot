493e9c1706537acad02b7b6564a1972452b12bbb
Denis Tsariov <tsariov.den@gmail.com>
2025-02-08 16:50:09 -0500

    430 tournament

--- main.py
+++ main.py
@@ -2,39 +2,50 @@
 import asyncio
 import argparse
 
+from tg.EV_bot import EV_bot
 from tg.bot import Bot
 import time
 
 parser = argparse.ArgumentParser(
-    prog='Template bot',
-    description='A Turing Games poker bot that always checks or calls, no matter what the target bet is (it never folds and it never raises)')
+    prog="Template bot",
+    description="A Turing Games poker bot that always checks or calls, no matter what the target bet is (it never folds and it never raises)",
+)
 
-parser.add_argument('--port', type=int, default=1999,
-                    help='The port to connect to the server on')
-parser.add_argument('--host', type=str, default='localhost',
-                    help='The host to connect to the server on')
-parser.add_argument('--room', type=str, default='my-new-room',
-                    help='The room to connect to')
-parser.add_argument('--username', type=str, default='bot',
-                    help='The username for this bot (make sure it\'s unique)')
+parser.add_argument(
+    "--port", type=int, default=1999, help="The port to connect to the server on"
+)
+parser.add_argument(
+    "--host", type=str, default="localhost", help="The host to connect to the server on"
+)
+parser.add_argument(
+    "--room", type=str, default="my-new-room", help="The room to connect to"
+)
+parser.add_argument(
+    "--username",
+    type=str,
+    default="freaks",
+    help="The username for this bot (make sure it's unique)",
+)
 
 args = parser.parse_args()
 
 cnt = 0
+
+
 # Always call
 class TemplateBot(Bot):
     def act(self, state, hand):
-        print('asked to act')
-        print('acting', state, hand, self.my_id)
-        return {'type': 'call'}
+        print("asked to act")
+        print("acting", state, hand, self.my_id)
+        return {"type": "call"}
 
     def opponent_action(self, action, player):
-        #print('opponent action?', action, player)
+        # print('opponent action?', action, player)
         pass
 
     def game_over(self, payouts):
         global cnt
-        #print('game over', payouts)
+        # print('game over', payouts)
         cnt += 1
         print(cnt)
 
@@ -42,6 +53,7 @@ class TemplateBot(Bot):
         self.my_id = my_id
         pass
 
+
 if __name__ == "__main__":
-    bot = TemplateBot(args.host, args.port, args.room, args.username)
+    bot = EV_bot(args.host, args.port, args.room, args.username)
     asyncio.run(bot.start())
--- tg/EV_bot.py
+++ tg/EV_bot.py
@@ -8,69 +8,77 @@ from tg.bot import Bot
 import tg.types as pokerTypes
 
 parser = argparse.ArgumentParser(
-    prog='Template bot',
-    description='A Turing Games poker bot that always checks or calls, no matter what the target bet is (it never folds and it never raises)')
+    prog="Template bot",
+    description="A Turing Games poker bot that always checks or calls, no matter what the target bet is (it never folds and it never raises)",
+)
 
-parser.add_argument('--port', type=int,
-                    help='The port to connect to the server on')
-parser.add_argument('--host', type=str, default='localhost',
-                    help='The host to connect to the server on')
-parser.add_argument('--room', type=str, default='my-new-room',
-                    help='The room to connect to')
-parser.add_argument('--party', type=str, default='poker',
-                    help='The room to connect to')
-parser.add_argument('--key', type=str, default='',
-                    help='The key for authentication')
-parser.add_argument('--simulations', type=int, default=900)
+parser.add_argument("--port", type=int, help="The port to connect to the server on")
+parser.add_argument(
+    "--host", type=str, default="localhost", help="The host to connect to the server on"
+)
+parser.add_argument(
+    "--room", type=str, default="my-new-room", help="The room to connect to"
+)
+parser.add_argument("--party", type=str, default="poker", help="The room to connect to")
+parser.add_argument("--key", type=str, default="", help="The key for authentication")
+parser.add_argument("--simulations", type=int, default=900)
 
 args = parser.parse_args()
+
 
 def card_name(card: pokerTypes.Card):
     val = str(card.rank)
     if card.rank == 1:
-        val = 'A'
+        val = "A"
     if card.rank == 10:
-        val = 'T'
+        val = "T"
     elif card.rank == 11:
-        val = 'J'
+        val = "J"
     elif card.rank == 12:
-        val = 'Q'
+        val = "Q"
     elif card.rank == 13:
-        val = 'K'
+        val = "K"
     return f"{val}{card.suit[0]}"
+
 
 class EV_bot(Bot):
     def act(self, state, hand):
-        print('asked to act')
-        print('acting', state, hand, self.my_id)
+        print("asked to act")
+        # print("acting", state, hand, self.my_id)
+        print(hand)
         p = self.win_prob(state, hand)
-        EV_Call = p*(state.pot+state.target_bet)-(state.target_bet)*(1-p)
-        best_val = min(0, EV_Call)
-        action = 'fold' if best_val == 0 else 'call'
-        return {'type': action}
+        print(p)
+        EV_Call = p * (state.pot + state.target_bet) - (state.target_bet) * (1 - p)
+        print(EV_Call)
+        best_val = max(0, EV_Call)
+        action = "fold" if best_val == 0 else "call"
+        return {"type": action}
 
     def opponent_action(self, action, player):
-        print('opponent action?', action, player)
+        print("opponent action?", action, player)
 
     def game_over(self, payouts):
-        print('game over', payouts)
+        print("game over", payouts)
 
     def start_game(self, my_id):
         self.my_id = my_id
-        print('start game', my_id)
-    def win_prob(self, state: pokerTypes.PokerSharedState, hand: Tuple[pokerTypes.Card, pokerTypes.Card]):
+        print("start game", my_id)
+
+    def win_prob(
+        self,
+        state: pokerTypes.PokerSharedState,
+        hand: Tuple[pokerTypes.Card, pokerTypes.Card],
+    ):
         out = 0
-        hand = [
-            treys.Card.new(card_name(hand[0])),
-            treys.Card.new(card_name(hand[1]))]
+        hand = [treys.Card.new(card_name(hand[0])), treys.Card.new(card_name(hand[1]))]
         board = [treys.Card.new(card_name(card)) for card in state.cards]
         evaluator = treys.Evaluator()
         for i in range(args.simulations):
             deck = treys.Deck()
             deck.shuffle()
-            for card in hand+board:
+            for card in hand + board:
                 deck.cards.remove(card)
-            pred = board + deck.draw(5-len(board))
+            pred = board + deck.draw(5 - len(board))
             score = evaluator.evaluate(hand, pred)
             other = 10**9
 
@@ -79,4 +87,4 @@ class EV_bot(Bot):
                     other = min(other, evaluator.evaluate(deck.draw(2), pred))
             if score < other:
                 out += 1
-        return out/args.simulations
+        return out / args.simulations
